bbd799ad4928470bc9683013c7c358770165d3ea
Michal Jarski <michal@jarski.eu>
2020-04-17 09:55:20 +0200

    Resolve merge conflicts

--- src/test/java/TestAll.java
+++ src/test/java/TestAll.java
@@ -13,7 +13,7 @@ import java.util.List;
  *
  * @author Tomasz Darmetko
  */
-public class AllTests extends SimpleUnitTest {
+public class TestAll extends SimpleUnitTest {
 
     public static void main(String[] args) {
         InteropTests.main(args);
